defee7cf391ec36b507cebc57df71d220d2f420a
Raphael ROMEO <95073137+Raph-Romeo@users.noreply.github.com>
2023-10-19 21:11:19 +0200

    Added style sheets
    - Styled the background of the window
    - Styled the Tab widget menu to look a bit more appealing

--- client application/main_window.py
+++ client application/main_window.py
@@ -18,3 +18,18 @@ class MainWindow(QMainWindow):
         grid.setSpacing(0)
         self.setCentralWidget(widget)
         grid.addWidget(self.mainTabWidget)
+        self.setStyleSheet("""
+    QWidget{background:transparent}
+    QMainWindow{background-color: qlineargradient(y1: 0, y2: 1, stop: 0 #f8f4f4, stop: 1 #e4e0e0)}
+    QTabWidget::pane {border:0px;background:transparent;} 
+    QTabBar::tab {background: rgb(10,10,10);border:none;padding:12px;color:#AAA;}
+    QTabBar::tab:hover {background:rgb(10,10,10);color:white;}
+    QTabBar::tab:selected {background:rgb(23,23,23);color:white;}
+    QScrollArea{border:none;}
+    QToolButton{background:transparent;border:none;font-family:verdana;font-weight:999;text-align:left;color:#000;font-size:12px;padding-left:15px;margin-top:14px;margin-bottom:14px}
+
+    QLabel{color:white;font-family:Consolas;font-size:15px;border:0px solid black;background-color:transparent;padding:0px;padding-left:10px;padding-right:10px;margin:0px;text-indent:0px;}
+    QLineEdit{border:0px solid black;margin:0px;padding:0px;font-family:Consolas;color:white;font-size:15px;padding-bottom:10px;text-indent:0px;margin-left:-2px;margin-top:-1px;}
+    
+    QLabel[title="true"]{color:black;margin-left:10px;font-size:20px;font-weight:1;font-family:verdana}
+""")
