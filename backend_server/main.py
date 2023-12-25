df83477f333aa6353839187c8e1097cda89d99d4
LAMINE-Lamine <92865842+LAMINE-Lamine@users.noreply.github.com>
2023-12-25 13:55:05 +0100

    main.py

--- backend_server/main.py
+++ backend_server/main.py
@@ -1,15 +1,27 @@
-import socket, sys
+import socket, sys, time
 
 class Server:
     def __init__(self):
         self.socket = socket.socket()
 
+    def close(self) -> None:
+        self.socket.close()
+        return
+
     def start(self) -> bool:
-        self.socket.bind(("0.0.0.0", 5240))
-        print(f"SERVER SUCCESSFULLY STARTED ON PORT : 5240")
+        try:
+            self.socket.bind(("0.0.0.0", 5240))
+        except socket.error as msg:
+            if self.number_of_attempts < self.max_number_of_attempts:
+                print(msg)
+                print("trying again in 5 seconds...")
+                time.sleep(5)
+                return self.start()
+        else:
+            print(f"SERVER SUCCESSFULLY STARTED ON PORT : 5240")
+            return True
 
 
 if __name__ == "__main__":
     server = Server()
     sys.exit(server.start())
-
