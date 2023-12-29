34d9a5e33e3e6e5c79cf365f81e6ca8916a6c3c9
Elmirbtj <92865807+Elmirbtj@users.noreply.github.com>
2023-12-29 23:56:04 +0100

    docker

---
+++ docker/deploy.sh
@@ -0,0 +1,31 @@
+if ! command -v docker &> /dev/null
+then
+    echo "Docker n'est pas installé. Installation en cours..."
+
+    if command -v apt-get &> /dev/null
+    then
+        sudo apt-get update
+        sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+    else
+        echo "Système d'exploitation non pris en charge. Veuillez installer Docker manuellement."
+        exit 1
+    fi
+fi
+
+if ! command -v docker-compose &> /dev/null
+then
+    echo "Docker Compose n'est pas installé. Installation en cours..."
+    sudo apt-get install -y docker-compose
+fi
+
+docker-compose down
+docker system prune -a --force --volumes
+
+docker network prune -f
+
+docker build -t task_master_pro_backend ../backend-server
+
+docker-compose up -d
+
+
+echo "Le déploiement est terminé."
